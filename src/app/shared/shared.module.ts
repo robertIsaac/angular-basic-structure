bcbd5186c3a5b2d94d9c4db00013198b8d9cbfeb
robertIsaac <robert.isaac@live.com>
2020-09-26 00:23:47 +0200

    add shared components. directives and pipes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedComponent } from './components/shared/shared.component';
+import { SharedDirective } from './directives/shared.directive';
+import { SharedPipe } from './pipes/shared.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SharedComponent,
+    SharedDirective,
+    SharedPipe,
+  ],
   imports: [
     CommonModule,
     FormsModule,
@@ -15,6 +22,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedComponent,
+    SharedDirective,
+    SharedPipe,
   ]
 })
 export class SharedModule { }
