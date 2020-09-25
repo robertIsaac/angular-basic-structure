6cba9b96178c46cc0a9de74d5ab00c19fc6a8d8b
robertIsaac <robert.isaac@live.com>
2020-09-25 23:57:54 +0200

    add shared module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+    SharedModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {
+}
--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { FeatureRoutingModule } from './feature-routing.module';
 import { FeatureComponent } from './feature.component';
-
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [FeatureComponent],
   imports: [
-    CommonModule,
-    FeatureRoutingModule
+    FeatureRoutingModule,
+    SharedModule,
   ]
 })
 export class FeatureModule { }
